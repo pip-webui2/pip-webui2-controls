7377212eb723e076b03d1c5252a7980e6af2353e
dvoikinalex <94dvoikin@gmail.com>
2017-10-19 15:25:32 +0300

    [Root index source] update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './test/index';
-export * from './ref-list';
-export * from './ref-item';
+export * from './ref-list/index';
+export * from './ref-item/index';
+export * from './color-picker/index';
